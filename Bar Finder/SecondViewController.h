4f366525da68421e180955bccc89ded6335efedf
P. Mark Anderson <mark@bordertownlabs.com>
2011-11-14 14:18:13 -0800

    Initial Commit

---
+++ Bar Finder/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Bar Finder
+//
+//  Created by P. Mark Anderson on 11/14/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
